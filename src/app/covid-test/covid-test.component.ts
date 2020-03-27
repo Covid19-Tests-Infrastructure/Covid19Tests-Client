f4063054b98fcf22bb4b5e643945c9e0b65e959b
LukasK-roboyor <lukas.klingebiel@web.de>
2020-03-27 13:12:37 +0100

    Update

--- src/app/covid-test/covid-test.component.ts
+++ src/app/covid-test/covid-test.component.ts
@@ -91,7 +91,7 @@ export class CovidTestComponent implements OnInit {
 			result => {
 				console.log(result);
 				this.snackbar.open("Erfolg!", "OK", { duration: 3000 });
-				this.router.navigate(["/covid-test"]);
+				this.router.navigate(["/"]);
 			},
 			error => {
 				console.log(error);
