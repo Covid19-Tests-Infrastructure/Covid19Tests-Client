c028bfed5392b33d7fc6a0499fb6a5d157b8930c
LukasK-roboyor <lukas.klingebiel@web.de>
2020-03-29 15:10:52 +0200

    Close Sidenav after navigation in handset mode

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { AuthService } from "../auth/services/auth.service";
 import { UserDto } from "../../../api";
 import { Observable } from "rxjs";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { map, shareReplay } from "rxjs/operators";
+import { map, shareReplay, withLatestFrom, filter } from "rxjs/operators";
+import { Router, NavigationEnd } from "@angular/router";
+import { MatSidenav } from "@angular/material/sidenav";
 
 @Component({
 	selector: "app-navigation",
@@ -14,14 +16,22 @@ export class NavigationComponent {
 
 	loggedInUser: UserDto;
 
+	@ViewChild("drawer") drawer: MatSidenav;
 	isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.Handset])
 		.pipe(
 			map(result => result.matches),
 			shareReplay()
 		);
 
+
 	constructor(private authService: AuthService,
-				private breakpointObserver: BreakpointObserver) { }
+				private breakpointObserver: BreakpointObserver,
+				private router: Router) {
+		router.events.pipe(
+			withLatestFrom(this.isHandset$),
+			filter(([a, b]) => b && a instanceof NavigationEnd)
+		).subscribe(x => this.drawer.close());
+	}
 
 	getUsername(): string {
 		return this.authService.getUserSettings()?.username ?? "";
