27e61b363ceec0ed5614e0fa6385e8620f978a33
LukasK-roboyor <lukas.klingebiel@web.de>
2020-06-23 19:04:17 +0200

    Add dynamic link to form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth/services/auth.service";
+import { UserDto } from "../../../api";
 
 @Component({
 	selector: "app-home",
@@ -7,9 +9,29 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	constructor(private authService: AuthService) { }
 
 	ngOnInit(): void {
+	}
+
+	/** Returns an absolute link to the order form corresponding the user's role. */
+	getLink(): string {
+		const role = this.authService.getUserSettings()?.role;
+
+		if (!role) return "login";
+
+		switch (role) {
+		case UserDto.RoleEnum.DEFAULT:
+			return "/covid-test-allgemein"; 
+		case UserDto.RoleEnum.KVN:
+			return "/covid-test";
+		case UserDto.RoleEnum.PRIVATE:
+			return "/covid-test-privat"; 
+		case UserDto.RoleEnum.ADMIN:
+			return "/covid-test-allgemein"; 
+		default:
+			return "";
+		}
 	}
 
 }
